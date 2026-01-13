29fdef2caf9a79df1bc690bd03e8ea52108b0b04
Hilal Serindik <serindikhilal1@gmail.com>
2026-01-13 13:41:10 +0100

    feat: add POST /books endpoint with auto-incremented id

--- bookDemo/Controllers/BooksController.cs
+++ bookDemo/Controllers/BooksController.cs
@@ -1,4 +1,5 @@
 ﻿using bookDemo.Data;
+using bookDemo.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
@@ -12,16 +13,29 @@ namespace bookDemo.Controllers
         [HttpGet]
         public IActionResult GetBooks()
         {
-            return Ok(Data.ApplicationContext.Books);
+            return Ok(ApplicationContext.Books);
         }
 
         [HttpGet]
         [Route("{id:int}")]
         public IActionResult GetBookById([FromRoute(Name = "id")] int id)
         {
-            var book = Data.ApplicationContext.Books.FirstOrDefault(b => b.Id == id);
+            var book = ApplicationContext.Books.FirstOrDefault(b => b.Id == id);
             return book is null ? NotFound() : Ok();
 
+        }
+        [HttpPost]
+        public IActionResult AddBook([FromBody] Book book)
+        {
+            if (book is null)
+            
+                return BadRequest("Book can not be null");
+
+            book.Id = ApplicationContext.Books.Any()
+                ? ApplicationContext.Books.Max(b => b.Id) + 1
+                : 1;
+            ApplicationContext.Books.Add(book);
+            return CreatedAtAction(nameof(GetBookById), new { id = book.Id }, book);
         }
     }
 }
