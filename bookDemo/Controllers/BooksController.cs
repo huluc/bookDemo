1b5c70e0c348079d3a8db10328065f2f4eb1b1da
Hilal Serindik <serindikhilal1@gmail.com>
2026-01-13 12:36:54 +0100

    feat: add Books API with in-memory data context

---
+++ bookDemo/Controllers/BooksController.cs
@@ -0,0 +1,27 @@
+﻿using bookDemo.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bookDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetBooks()
+        {
+            return Ok(Data.ApplicationContext.Books);
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public IActionResult GetBookById([FromRoute(Name = "id")] int id)
+        {
+            var book = Data.ApplicationContext.Books.FirstOrDefault(b => b.Id == id);
+            return book is null ? NotFound() : Ok();
+
+        }
+    }
+}
