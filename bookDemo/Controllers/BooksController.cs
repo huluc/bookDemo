328b41ba72a61e95969f235fd53536ecf7dfc411
Hilal Serindik <serindikhilal1@gmail.com>
2026-01-16 14:32:30 +0100

    feat(books): add delete book and delete all books endpoints

--- bookDemo/Controllers/BooksController.cs
+++ bookDemo/Controllers/BooksController.cs
@@ -21,14 +21,14 @@ namespace bookDemo.Controllers
         public IActionResult GetBookById([FromRoute(Name = "id")] int id)
         {
             var book = ApplicationContext.Books.FirstOrDefault(b => b.Id == id);
-            return book is null ? NotFound() : Ok();
+            return book is null ? NotFound() : Ok(book);
 
         }
         [HttpPost]
         public IActionResult AddBook([FromBody] Book book)
         {
             if (book is null)
-            
+
                 return BadRequest("Book can not be null");
 
             book.Id = ApplicationContext.Books.Any()
@@ -37,5 +37,43 @@ namespace bookDemo.Controllers
             ApplicationContext.Books.Add(book);
             return CreatedAtAction(nameof(GetBookById), new { id = book.Id }, book);
         }
+        [HttpPut("{id:int}")]
+        public IActionResult UpdateBook([FromRoute] int id, [FromBody] Book book)
+        {
+            if (book is null)
+                return BadRequest("Book cannot be null");
+
+            if (id != book.Id)
+                return BadRequest("Book ID mismatch");
+
+            var existing = ApplicationContext.Books.FirstOrDefault(b => b.Id == id);
+            if (existing is null)
+                return NotFound();
+
+            existing.Title = book.Title;
+            existing.Price = book.Price;
+
+            return NoContent(); // 204
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteBook([FromRoute] int id)
+        {
+            var book = ApplicationContext.Books.FirstOrDefault(b => b.Id == id);
+            if (book is null)
+                return NotFound(new
+                {
+                    Message = $"Book with ID {id} not found."
+                });
+            ApplicationContext.Books.Remove(book);
+            return NoContent(); // 204
+        }
+        [HttpDelete]
+        public IActionResult DeleteAllBooks()
+        {
+            ApplicationContext.Books.Clear();
+            return NoContent(); // 204
+        }
+
     }
 }
