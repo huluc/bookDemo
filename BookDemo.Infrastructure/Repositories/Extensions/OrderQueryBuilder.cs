ebb78186c5125f69a00a68dea25d8754c0a2348a
Hilal Serindik <serindikhilal1@gmail.com>
2026-04-27 11:49:33 +0200

    refactor: Sorting logic into seperate query builder
    - Extract order query creation into OrderQueryBuilder.
    - Make query builder generic.
    - Improve separation of concerns in sorting logic.

---
+++ BookDemo.Infrastructure/Repositories/Extensions/OrderQueryBuilder.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace BookDemo.Infrastructure.Repositories.Extensions
+{
+    public static class OrderQueryBuilder
+    {
+        public static String CreateOrderQuery<T>(string orderByQueryString)
+        {
+            var orderParams = orderByQueryString.Trim().Split(',');
+
+            var propertyInfos = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            var orderQueryBuilder = new StringBuilder();
+
+            foreach (var param in orderParams)
+            {
+                if (string.IsNullOrWhiteSpace(param))
+                    continue;
+
+                var propertyFromQueryName = param.Trim().Split(' ')[0];
+                var objectProperty = propertyInfos.FirstOrDefault(pi => pi.Name.Equals(propertyFromQueryName, StringComparison.InvariantCultureIgnoreCase));
+
+                if (objectProperty is null)
+                    continue;
+
+                var direction = param.Trim()
+                    .EndsWith(" desc", StringComparison.InvariantCultureIgnoreCase)
+                    ? "descending"
+                    : "ascending";
+
+                orderQueryBuilder.Append($"{objectProperty.Name} {direction}, ");
+            }
+
+            var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
+            return orderQuery;
+        }
+    }
+}
