fd05e9dd2a5c057eca0fecd4b964b109d1a825ca
huluc <40721928+huluc@users.noreply.github.com>
2026-05-21 14:03:44 +0200

    feat: implement HATEOAS root endpoint with LinkGenerator and custom media types (#28)
    - Introduce RootController with LinkGenerator for dynamic URL generation
    - Introduce MediaTypes constants class in Application layer
    - Register custom media types (hateoas+json, hateoas+xml, apiroot+json, apiroot+xml) in ServiceExtension

---
+++ BookDemo.Application/Constants/MediaTypes.cs
@@ -0,0 +1,11 @@
+﻿namespace BookDemo.Application.Constants
+{
+    public static class MediaTypes
+    {
+        public const string ApiRoot = "application/vnd.hilal.bookdemo.apiroot";
+        public const string HateoasJson = "application/vnd.hilal.bookdemo.hateoas+json";
+        public const string HateoasXml = "application/vnd.hilal.bookdemo.hateoas+xml";
+        public const string ApiRootJson = "application/vnd.hilal.bookdemo.apiroot+json";
+        public const string ApiRootXml = "application/vnd.hilal.bookdemo.apiroot+xml";
+    }
+}
